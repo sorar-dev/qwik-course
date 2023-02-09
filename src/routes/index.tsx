b021262b2430acfd0f13c4c0eb2f3296bd5ab843
ckechad <ckechad@icloud.com>
2023-02-09 20:01:59 +0100

    hello world in qwik course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,2 +1,11 @@
+import {component$} from "@builder.io/qwik";
 
+export default component$(() => {
+    console.log('Initializing Hello World component!')
+    return (
+        <h1>
+            Welcome to this Qwik Course!
+        </h1>
+    )
+});
 
